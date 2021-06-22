cae2328e1fe5a91ce61ea366636778f17cb63916
Vincenzo Stramglia <vcento@libero.it>
2021-06-22 06:44:20 +0200

    code cleaning

--- Extensions/Extension.DialogResult.cs
+++ Extensions/Extension.DialogResult.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vi.Tools.Extensions.DialogResult
+﻿namespace Vi.Tools.Extensions.DialogResult
 {
     /// <summary>
     /// Collection of 'extension methods' for DialogResult
